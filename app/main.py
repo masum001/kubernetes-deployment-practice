80aeb95453bebc1dc66ed692d6e03e70635747db
sultan101 <masumkhalil7@gmail.com>
2025-01-13 09:23:42 +0600

    change in main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import os
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Kubernetes"}
+    return {"Hello": f"from: {os.environ.get('ENV', 'DEFAULT_ENV')}"}
 
 
